8040d0d888b0da56b92bf1e49029a85d89e8ccee
jrpe1192 <96844622+jrpe1192@users.noreply.github.com>
2022-02-02 09:01:49 -0500

    Update shaper.sh

--- shaper.sh
+++ shaper.sh
@@ -2,6 +2,6 @@
 
 for i in `ip a | grep veth | awk '{print $2}' | tr ':' ' ' | tr '@' ' ' | awk '{print $1}'`
 do
-echo "wondershaper $i 10240 10240".
-wondershaper $i 10240 10240
+echo "wondershaper $i 20480 20480".
+wondershaper $i 20480 20480
 done
